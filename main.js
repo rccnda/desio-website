e854b2d4d1b47b28c6d1b36e3a9d01cb62d5b530
Nadia Ruocco <nadia.ruocco@cleverbase.com>
2025-06-21 16:25:39 +0200

    fix: map

--- main.js
+++ main.js
@@ -192,8 +192,11 @@ document.addEventListener('DOMContentLoaded', function() {
                     scrollWheelZoom: false // Optional: disable scroll wheel zoom
                 }).setView([data.lat, data.lng], 18);
 
-                L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png', {
-                    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.{ext}', {
+                    minZoom: 0,
+                    maxZoom: 18,
+                    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                    ext: 'png'
                 }).addTo(map);
 
                 const customMarker = L.icon({
