c1df4199b5d19a821c9ba3d69a92f22ae2936bbb
Nadia Ruocco <nadia.ruocco@cleverbase.com>
2025-07-16 18:54:44 +0200

    feat: footer

--- footer-inject.js
+++ footer-inject.js
@@ -1,5 +1,10 @@
 fetch('footer.html')
   .then(response => response.text())
   .then(html => {
-    document.body.insertAdjacentHTML('beforeend', html);
+    const placeholder = document.getElementById('footer-placeholder');
+    if (placeholder) {
+      placeholder.innerHTML = html;
+    } else {
+      document.body.insertAdjacentHTML('beforeend', html);
+    }
   }); 
